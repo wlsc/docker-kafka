71d57f926fc434c0b8c34f76f9da9133ec992c2f
Wladimir Schmidt <wlsc.dev@gmail.com>
2022-12-29 16:50:01 +0100

    Add configuration for listeners

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # Optional ENV variables:
+# * ADVERTISED_LISTENERS: your local machine IP address (or docker machine IP)
+# * INTER_BROKER_LISTENER_NAME: mame of a listener used for communication between brokers
+# * LISTENER_SECURITY_PROTOCOL_MAP: map between listener names and security protocols
 # * LOG_RETENTION_HOURS: the minimum age of a log file in hours to be eligible for deletion (default is 168, for 1 week)
 # * LOG_RETENTION_BYTES: configure the size at which segments are pruned from the log, (default is 1073741824, for 1GB)
 # * NUM_PARTITIONS: configure the default number of log partitions per topic
@@ -8,6 +11,21 @@
 
 # add carriage return so appended properties don't end up appended to the end of the last line
 echo "" >> $KAFKA_HOME/config/kraft/server.properties
+
+if [[ ! -z "$ADVERTISED_LISTENERS" ]]; then
+  echo "advertised listeners: ${ADVERTISED_LISTENERS}"
+  sed -r -i "s@^#?advertised.listeners=.*@advertised.listeners=$ADVERTISED_LISTENERS@g" $KAFKA_HOME/config/kraft/server.properties
+fi
+
+if [[ ! -z "$INTER_BROKER_LISTENER_NAME" ]]; then
+  echo "inter broker listener name: ${INTER_BROKER_LISTENER_NAME}"
+  sed -r -i "s@^#?inter.broker.listener.name=.*@inter.broker.listener.name=$INTER_BROKER_LISTENER_NAME@g" $KAFKA_HOME/config/kraft/server.properties
+fi
+
+if [[ ! -z "$LISTENER_SECURITY_PROTOCOL_MAP" ]]; then
+  echo "listener security protocol map: ${LISTENER_SECURITY_PROTOCOL_MAP}"
+  sed -r -i "s@^#?listener.security.protocol.map=.*@listener.security.protocol.map=$LISTENER_SECURITY_PROTOCOL_MAP@g" $KAFKA_HOME/config/kraft/server.properties
+fi
 
 # Allow specification of log retention policies
 if [ ! -z "$LOG_RETENTION_HOURS" ]; then
